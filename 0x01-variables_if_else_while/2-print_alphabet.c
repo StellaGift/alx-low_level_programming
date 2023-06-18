e0db7f5f9436e8b5262ca44aeaa030c60704cd4c
StellaGift <giftada151@gmail.com>
2023-06-18 06:33:53 -0700

    putchar

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
-  *main - Entry point
-  *Description: 'prints alphabet lowercase'
-  *Return: always 0
-*/
+ *main - Entry point
+ *Description: 'prints alphabets lowercase'
+ *Return: always 0
+ */
 int main(void)
 {
 	putchar(65);
