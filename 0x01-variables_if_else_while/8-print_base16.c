93b39820cdfb269bd9860c99c0aaf79766015099
StellaGift <giftada151@gmail.com>
2023-06-18 12:14:35 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Description: 'print lowercase numbers of base 16'
+ *Return: Always 0
+ */
+int main(void)
+{
+	int n;
+	int s;
+	
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (s = 97; s <= 102; s++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}		
