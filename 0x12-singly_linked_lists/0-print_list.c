2ca18d4c0e04f01c5c867f73c97e223b834f5853
StellaGift <giftada151@gmail.com>
2023-07-28 10:14:48 +0000

    prints all the elements of a linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,7 +2,7 @@
 #include "lists.h"
 
 /**
- * prints_list - prints all the element of a linked list
+ * prints_list - prints all the elements of a linked list
  * @h: pointer to the list_t list to print
  *
  * Return: the number of nodes printed
