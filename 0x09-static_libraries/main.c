47cccc9c93910a57d5655982cb732210118ceff5
StellaGift <119241329+StellaGift@users.noreply.github.com>
2023-08-30 15:48:19 +0100

    Deleted 0x09-static_libraries directory

--- 0x09-static_libraries/main.c
+++
@@ -1,7 +0,0 @@
-#include "main.h"
-
-int main(void)
-{
-    _puts("\"At the end of the day, my goal was to be the best ethical hacker\\pentester and programmer\"\n\t- Stella Gift Oriji");
-    return (0);
-}
